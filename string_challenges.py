b7fa3ddd23b2975cbf8150f2d19b47b71e064e75
Lana Novikova <svetlnovikova@gmail.com>
2021-09-14 00:16:59 +0300

    basic exercises: string and fors

--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,37 @@
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
-
+list_of_letters = list(word)
+print(list_of_letters[-1])
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
-
+list_of_letters = list(word)
+print(list_of_letters.count('а'))
 
 # Вывести количество гласных букв в слове
-word = 'Архангельск'
-# ???
-
+word = 'Архангёльск'
+vowels = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
+number_vowels=0
+for letter in word:
+    if letter.lower() in vowels:
+        number_vowels = number_vowels+1
+print(number_vowels)
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-# ???
-
+print(len(sentence.split()))
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
-
+words = sentence.split()
+print(words)
+for word in words:
+    print(word[0])
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+words = sentence.split()
+print(words)
+
+average_length = sum(len(word) for word in words)/len(sentence)
+print(average_length)
